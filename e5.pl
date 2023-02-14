1319c5eecc813c6ded29affc09b6824946b061e8
Aurelio Marín Bautista <72474524+vKurama7u7v@users.noreply.github.com>
2023-02-14 17:19:54 -0600

    Create e5.pl

---
+++ e5.pl
@@ -0,0 +1,28 @@
+% BASE DE CONOCIMIENTOS
+puede(animal, respirar).
+es_un(ave, animal).
+es_un(mamifero, animal).
+
+vuela(ave, bien).
+tiene(ave, pluna).
+pone(ave, huvos).
+
+tiene(mamifero, pelo).
+da(mamifero, leche).
+
+es_un(avestruz, ave).
+es_un(albatros, ave).
+tiene_patas(avestruz, largas).
+vuela(avestruz, no_puede).
+vuela(albatros, muy_bien).
+
+es_un(ballena, mamifero).
+es_un(tigre, mamifero).
+
+tiene(ballena, piel).
+vive_en(ballena, mar).
+
+come(tigre, carne).
+
+
+
